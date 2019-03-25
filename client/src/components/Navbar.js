8ea775acbdb36dbf2c4d0ce954040cbf4daafdd1
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-25 15:41:31 +0100

    Style Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
     <header>
       <nav>
-        <figure>
-          <img className="logo" src="img/job-huntr-icon.png" alt="logo" />
-        </figure>
+        <section className="logo-container">
+          <div className="logo-border">
+            <figure>
+              <img className="logo" src="img/job-huntr-icon.png" alt="logo" />
+            </figure>
+          </div>
+        </section>
       </nav>
     </header>
   );
