d2e93eda474cedc7d0e62ef417633fe4646d14e0
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-26 12:14:23 +0100

    Pass correct title to Navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 
+export const appConfig = {
+  title: 'Job Huntr'
+};
+
 const App = () => {
   return (
     <div>
-      <Navbar />
+      <Navbar title={appConfig.title} />
     </div>
   );
 };
