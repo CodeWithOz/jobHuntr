0386169cef8ad21bbdf8da4a1cc2823b167a9644
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-26 12:09:31 +0100

    Test if App correctly assigns title to Navbar

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from './App';
+import App, { appConfig } from './App';
 import Navbar from './Navbar';
 
 describe('App renders', () => {
   test('a Navbar', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(Navbar).length).toEqual(1);
+  });
+});
+
+describe('App correctly assigns', () => {
+  test('Navbar title', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Navbar).prop('title')).toEqual(appConfig.title);
   });
 });
