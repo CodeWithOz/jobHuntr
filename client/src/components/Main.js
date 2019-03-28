5d76363bdb305adfad1f471bd66007ec14f4f81b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-28 18:48:34 +0100

    Render a Route to LandingPage

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Route exact path="/" component={LandingPage} />
+    </div>
+  );
 };
 
 export default Main;
