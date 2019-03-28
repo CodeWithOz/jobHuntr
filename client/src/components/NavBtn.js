c1b6536ab303733407ec85ddcdbfe7a11e0d8ca8
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-28 18:17:15 +0100

    Add prop types for NavBtn

--- client/src/components/NavBtn.js
+++ client/src/components/NavBtn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const NavBtn = ({ type }) => {
   return (
@@ -6,6 +7,10 @@ const NavBtn = ({ type }) => {
       <button className="navbtn">{type}</button>
     </li>
   );
+};
+
+NavBtn.propTypes = {
+  type: PropTypes.string
 };
 
 export default NavBtn;
