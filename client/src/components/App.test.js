5c444e852453881f980e112771cfef77b2be4105
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-28 18:29:45 +0100

    Test if App uses HashRouter for routing

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { HashRouter } from 'react-router-dom';
 import App, { appConfig } from './App';
 import Navbar from './Navbar';
 
 describe('App renders', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
   test('a Navbar', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Navbar).length).toEqual(1);
+  });
+
+  test('a HashRouter for URL routing', () => {
+    expect(wrapper.find(HashRouter).length).toEqual(1);
   });
 });
 
