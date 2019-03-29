f030a5b86fa8efc52f56fa20408d1a271667684a
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-29 16:45:34 +0100

    Style CTA text

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -8,17 +8,23 @@ export const landingPageConfig = {
 
 const LandingPage = () => {
   return (
-    <section>
-      <Button type="login" />
-      <p>{landingPageConfig.cta1}</p>
-      <p>{landingPageConfig.cta2}</p>
-      <figure>
-        <img
-          className="preview-img"
-          src="https://via.placeholder.com/150/3d3479/ff3569.png?text=Preview"
-          alt="preview"
-        />
-      </figure>
+    <section className="landing-page">
+      <section className="cta-container first-cta">
+        <div className="text">
+          <p className="cta cta-1">{landingPageConfig.cta1}</p>
+          <p className="cta cta-2">{landingPageConfig.cta2}</p>
+          <Button type="login" />
+        </div>
+        <div className="image">
+          <figure>
+            <img
+              className="preview-img"
+              src="https://via.placeholder.com/150/3d3479/ff3569.png?text=Preview"
+              alt="preview"
+            />
+          </figure>
+        </div>
+      </section>
     </section>
   );
 };
