7b1325fb19bb7bdd6e22f1395705ffddf77159c9
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-29 09:17:02 +0100

    Test if LandingPage renders a preview image

--- client/src/components/pages/LandingPage.test.js
+++ client/src/components/pages/LandingPage.test.js
@@ -4,9 +4,19 @@ import LandingPage from './LandingPage';
 import Button from '../Button';
 
 describe('Landing page renders', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<LandingPage />);
+  });
+
   test('a login button', () => {
-    const wrapper = shallow(<LandingPage />);
     expect(wrapper.find({ type: 'login' }).length).toEqual(1);
     expect(wrapper.find({ type: 'login' }).is(Button)).toEqual(true);
+  });
+
+  test('a preview image', () => {
+    expect(wrapper.find('.preview-img').length).toEqual(1);
+    expect(wrapper.find('.preview-img').is('img')).toEqual(true);
   });
 });
