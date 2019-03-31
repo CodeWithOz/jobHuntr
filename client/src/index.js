d6980e4a14b6d70a11e10957d1d59dbfcd47cb54
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 19:04:59 +0100

    Configure redux and redux-devtools (not working)
    No reducers have been defined so this change breaks
    the app.

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
 import App from './components/App';
 import './sass/main.scss';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
