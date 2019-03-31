0b39e0dae8f5d403417e34d417b91b9a9498ddbb
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 22:58:32 +0100

    Test INITIAL_STATE's value of navMenuShown

--- client/src/constants/index.test.js
+++ client/src/constants/index.test.js
@@ -1,5 +1,9 @@
-import { TOGGLE_NAV_MENU } from './';
+import { INITIAL_STATE, TOGGLE_NAV_MENU } from './';
 
 test('TOGGLE_NAV_MENU has the correct value', () => {
   expect(TOGGLE_NAV_MENU).toEqual('TOGGLE_NAV_MENU');
+});
+
+test('INITIAL_STATE has the correct navMenuShown value', () => {
+  expect(INITIAL_STATE.navMenuShown).toEqual(false);
 });
