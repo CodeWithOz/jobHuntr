7df08b6fb99f6710b5817594de3be80f06d1dad8
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 23:41:03 +0100

    Test if MenuIcon handles clicks
    The test now imports a named import because the default
    import will be the redux-connected component, which
    will not work well with these tests.

--- client/src/components/MenuIcon.test.js
+++ client/src/components/MenuIcon.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import MenuIcon from './MenuIcon';
+import { MenuIcon } from './MenuIcon';
 
 describe('MenuIcon renders', () => {
   test('a Font Awesome icon', () => {
     const wrapper = shallow(<MenuIcon />);
     expect(wrapper.find('.fas.fa-bars').length).toEqual(1);
   });
+});
+
+describe('MenuIcon calls handleClick when clicked', () => {
+  const mock = jest.fn();
+  const wrapper = shallow(<MenuIcon handleClick={mock} />);
+  expect(mock).not.toHaveBeenCalled();
+  wrapper.find('button').simulate('click');
+  expect(mock).toHaveBeenCalledTimes(1);
 });
