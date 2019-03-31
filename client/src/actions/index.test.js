25ab63b147185b8bfbff0ca3bbeb05a25413c8bd
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 20:59:10 +0100

    Test if thunk calls dispatch correctly

--- client/src/actions/index.test.js
+++ client/src/actions/index.test.js
@@ -1,4 +1,5 @@
 import { toggleNavMenu } from './';
+import { TOGGLE_NAV_MENU } from '../constants';
 
 describe('toggleNavMenu', () => {
   test('is a function', () => {
@@ -8,6 +9,23 @@ describe('toggleNavMenu', () => {
   describe('returns', () => {
     test('a function', () => {
       expect(typeof toggleNavMenu()).toEqual('function');
+    });
+
+    describe('a function that', () => {
+      describe('calls its first argument (dispatch) with', () => {
+        test('the correct type', () => {
+          const dispatch = jest.fn();
+          const thunk = jest.fn(toggleNavMenu());
+          expect(thunk).not.toHaveBeenCalled();
+          expect(dispatch).not.toHaveBeenCalled();
+
+          thunk(dispatch);
+          expect(dispatch).toHaveBeenCalledTimes(1);
+          const dispatchedAction = dispatch.mock.calls[0][0];
+          expect(dispatchedAction.type).toBeDefined();
+          expect(dispatchedAction.type).toEqual(TOGGLE_NAV_MENU);
+        });
+      });
     });
   });
 });
