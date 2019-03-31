85bf3c5e0d427cdbb37ab9741118e424f543bc76
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 15:33:18 +0100

    Bring back the menu icon component
    I intend to implement this in a different way from what I
    initially intended.

---
+++ client/src/components/MenuIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MenuIcon = () => {
+  return (
+    <button className="menu-icon" href>
+      <i className="fas fa-bars" />
+      <p>
+        <strong>menu</strong>
+      </p>
+    </button>
+  );
+};
+
+export default MenuIcon;
