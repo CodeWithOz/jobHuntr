4b4d88213556bdbe1f30f9d716c306af912a6ab3
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 20:13:48 +0100

    Test if toggleNavMenu returns the correct type

--- client/src/actions/index.test.js
+++ client/src/actions/index.test.js
@@ -1,7 +1,16 @@
 import { toggleNavMenu } from './';
+import { TOGGLE_NAV_MENU } from '../constants';
 
 describe('toggleNavMenu', () => {
   test('is a function', () => {
     expect(typeof toggleNavMenu).toEqual('function');
+  });
+
+  describe('returns', () => {
+    test('the correct type', () => {
+      const action = toggleNavMenu();
+      expect(action.type).toBeDefined();
+      expect(action.type).toEqual(TOGGLE_NAV_MENU);
+    });
   });
 });
