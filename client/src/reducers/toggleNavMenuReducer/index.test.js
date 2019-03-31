8f9285c578eb411975e9e3207efb97adb8a61b2c
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 23:10:42 +0100

    Test if reducer returns initial state by default

--- client/src/reducers/toggleNavMenuReducer/index.test.js
+++ client/src/reducers/toggleNavMenuReducer/index.test.js
@@ -1,7 +1,15 @@
 import toggleNavMenuReducer from './';
+import { INITIAL_STATE } from '../../constants';
 
 describe('toggleNavMenuReducer', () => {
   test('is a function', () => {
     expect(typeof toggleNavMenuReducer).toEqual('function');
+  });
+
+  describe('returns', () => {
+    test('the initial state by default', () => {
+      const state = toggleNavMenuReducer(undefined, {});
+      expect(state).toEqual(INITIAL_STATE.navMenuShown);
+    });
   });
 });
