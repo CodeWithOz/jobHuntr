7e461bcaa8eb5dfda3b8f8afe61a8f01d8fd0dc3
Uche Ozoemena <ucheozoemena@gmail.com>
2019-03-31 22:47:07 +0100

    Test if thunk dispatches the correct payload

--- client/src/actions/index.test.js
+++ client/src/actions/index.test.js
@@ -13,9 +13,14 @@ describe('toggleNavMenu', () => {
 
     describe('a function that', () => {
       describe('dispatches an action with', () => {
+        let dispatch, thunk;
+
+        beforeEach(() => {
+          dispatch = jest.fn();
+          thunk = jest.fn(toggleNavMenu());
+        });
+
         test('the correct type', () => {
-          const dispatch = jest.fn();
-          const thunk = jest.fn(toggleNavMenu());
           expect(thunk).not.toHaveBeenCalled();
           expect(dispatch).not.toHaveBeenCalled();
 
@@ -24,6 +29,19 @@ describe('toggleNavMenu', () => {
           const dispatchedAction = dispatch.mock.calls[0][0];
           expect(dispatchedAction.type).toBeDefined();
           expect(dispatchedAction.type).toEqual(TOGGLE_NAV_MENU);
+        });
+
+        test('payload reversed from previous state', () => {
+          expect(thunk).not.toHaveBeenCalled();
+          expect(dispatch).not.toHaveBeenCalled();
+
+          const navMenuShown = true;
+          const getState = jest.fn(() => {
+            return { navMenuShown };
+          });
+          thunk(dispatch, getState);
+          const dispatchedAction = dispatch.mock.calls[0][0];
+          expect(dispatchedAction.payload).toEqual(!navMenuShown);
         });
       });
     });
