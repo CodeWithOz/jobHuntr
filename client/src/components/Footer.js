c151d71f0536ff7a6384872c71a66d9baaf6b516
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-08 06:11:43 +0100

    Render a link to huntr.co

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const footerConfig = {
+  huntrPlug: 'Modeled after ',
+  huntrUrl: 'https://huntr.co'
+};
+
 const Footer = () => {
   return (
     <div>
       <hr />
-      Footer
+      <p>
+        {footerConfig.huntrPlug}
+        <a href={footerConfig.huntrUrl}>huntr.co</a>.
+      </p>
     </div>
   );
 };
