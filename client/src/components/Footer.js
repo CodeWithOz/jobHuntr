843f7199de3ea6041b98691f95e023b00ae5bdaa
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-08 06:25:47 +0100

    Render link to my LinkedIn

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,14 +3,20 @@ import React from 'react';
 const footerConfig = {
   huntrPlug: 'Modeled after ',
   huntrUrl: 'https://huntr.co',
-  githubPlug: 'Improve jobHuntr at ',
-  githubUrl: 'https://github.com/CodeWithOz/jobHuntr'
+  githubPlug: 'You can improve jobHuntr at ',
+  githubUrl: 'https://github.com/CodeWithOz/jobHuntr',
+  linkedinPlug: 'Made by ',
+  linkedinUrl: 'https://www.linkedin.com/in/uchechukwu-ozoemena/'
 };
 
 const Footer = () => {
   return (
     <div>
       <hr />
+      <p>
+        {footerConfig.linkedinPlug}
+        <a href={footerConfig.linkedinUrl}>Uchechukwu Ozoemena</a>.
+      </p>
       <p>
         {footerConfig.huntrPlug}
         <a href={footerConfig.huntrUrl}>huntr.co</a>.
