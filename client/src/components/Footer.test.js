3483102134a4992df5575024d90e3ff2cbfb4852
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-08 06:01:36 +0100

    Test if Footer renders a link to huntr.co

--- client/src/components/Footer.test.js
+++ client/src/components/Footer.test.js
@@ -3,8 +3,17 @@ import { shallow } from 'enzyme';
 import Footer from './Footer';
 
 describe('Footer renders', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Footer />);
+  });
+
   test('a horizontal rule', () => {
-    const wrapper = shallow(<Footer />);
     expect(wrapper.find('hr').length).toEqual(1);
+  });
+
+  test('a link to huntr.com', () => {
+    expect(wrapper.find('[href="https://huntr.co"]').length).toEqual(1);
   });
 });
