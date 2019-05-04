2202858b8be3bd9942adb274f4a61d1bba9ad13e
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-04 18:13:29 +0100

    Test if a route exists for the demo page

--- client/src/components/Main.test.js
+++ client/src/components/Main.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import { Route } from 'react-router-dom';
 import Main from './Main';
 import LandingPage from './pages/LandingPage';
+import DemoPage from './pages/DemoPage';
 
 describe('Main renders', () => {
   describe('a Route', () => {
@@ -10,6 +11,12 @@ describe('Main renders', () => {
       const wrapper = shallow(<Main />);
       expect(wrapper.find({ component: LandingPage }).length).toEqual(1);
       expect(wrapper.find({ component: LandingPage }).is(Route)).toEqual(true);
+    });
+
+    test('whose component is DemoPage', () => {
+      const wrapper = shallow(<Main />);
+      expect(wrapper.find({ component: DemoPage }).length).toEqual(1);
+      expect(wrapper.find({ component: DemoPage }).is(Route)).toEqual(true);
     });
   });
 });
