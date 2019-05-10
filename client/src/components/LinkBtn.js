56ac778a24c5f86be074c0fb103d9b2f16def93a
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-10 23:10:03 +0100

    Add propTypes for LinkBtn

--- client/src/components/LinkBtn.js
+++ client/src/components/LinkBtn.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const LinkBtn = ({ children, className }) => {
   return <button className={className}>{children}</button>;
+};
+
+LinkBtn.propTypes = {
+  className: PropTypes.string
 };
 
 export default LinkBtn;
