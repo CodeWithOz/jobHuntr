169723411bf7d629672294bc8e3dd095e518c67b
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-10 23:05:51 +0100

    Test if LinkBtn passes props to its button

--- client/src/components/LinkBtn.test.js
+++ client/src/components/LinkBtn.test.js
@@ -7,4 +7,13 @@ describe('LinkBtn renders', () => {
     const wrapper = shallow(<LinkBtn />);
     expect(wrapper.find('button').length).toEqual(1);
   });
+
+  test('a button that gets the props passed to LinkBtn', () => {
+    const className = 'test',
+      children = 'test',
+      wrapper = shallow(<LinkBtn className={className}>{children}</LinkBtn>);
+
+    expect(wrapper.find('button').prop('children')).toEqual(children);
+    expect(wrapper.find('button').prop('className')).toEqual(className);
+  });
 });
