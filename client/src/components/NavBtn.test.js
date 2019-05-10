34dfcf3e4e49c26aef541208952a83bd0f2e2814
Uche Ozoemena <ucheozoemena@gmail.com>
2019-05-10 19:13:44 +0100

    Test if NavBtn conditionally renders a LinkBtn

--- client/src/components/NavBtn.test.js
+++ client/src/components/NavBtn.test.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NavBtn from './NavBtn';
+import LinkBtn from './LinkBtn';
 
 describe('NavBtn renders', () => {
   test('its text prop', () => {
     const text = 'test';
     const wrapper = shallow(<NavBtn text={text} />);
     expect(wrapper.text()).toContain(text);
+  });
+
+  test('a LinkBtn according to isLink prop', () => {
+    const wrapper = shallow(<NavBtn isLink />);
+    expect(wrapper.find(LinkBtn).length).toEqual(1);
+
+    wrapper.setProps({ isLink: false });
+    expect(wrapper.find(LinkBtn).length).toEqual(0);
   });
 });
